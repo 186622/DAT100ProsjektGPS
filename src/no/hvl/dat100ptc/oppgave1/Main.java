6109ad8bef88380591ed5ff7ca9d8ad6a448bc09
HanssenGLRP <66432612+186622@users.noreply.github.com>
2024-10-04 10:15:49 +0200

    Oppgave 1: D)
    add: GPSPoint Objekt i main.java

--- src/no/hvl/dat100ptc/oppgave1/Main.java
+++ src/no/hvl/dat100ptc/oppgave1/Main.java
@@ -3,8 +3,14 @@ package no.hvl.dat100ptc.oppgave1;
 public class Main {
 
 	public static void main(String[] args) {
+		GPSPoint point = new GPSPoint(1, 2.0, 3.0, 5.0);
+		point.setTime(2);
+
+		System.out.println("Tidspunkt: " + point.getTime());
 		
-		// TODO
+		
+		System.out.println(point.toString());
+	
 		
 	}
 
